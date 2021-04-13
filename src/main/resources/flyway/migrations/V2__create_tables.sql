ad29de42db7652fc55b9766482dc8e7cfcc1a130
TheVestedLeopard <sachajw@gmail.com>
2021-04-13 20:50:12 +0200

    cleaning up

--- src/main/resources/flyway/migrations/V2__create_tables.sql
+++
@@ -1,9 +0,0 @@
-CRE
-CREATE TABLE petclinic.owners (
-    id INT NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE petclinic.vets (
-   id INT NOT NULL PRIMARY KEY
-);
-
--- src/main/resources/flyway/migrations/V3__alter_tables.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE petclinic.owners ADD COLUMN name VARCHAR(255);
-ALTER TABLE petclinic.owners ADD COLUMN address VARCHAR(255);
-ALTER TABLE petclinic.owners ADD COLUMN city VARCHAR(255);
-ALTER TABLE petclinic.owners ADD COLUMN telephone VARCHAR(255);
-ALTER TABLE petclinic.owners ADD COLUMN pets VARCHAR(255);
-
-ALTER TABLE petclinic.vets ADD COLUMN name VARCHAR(255);
-ALTER TABLE petclinic.vets ADD COLUMN specialties VARCHAR(255);
-
