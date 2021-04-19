975610aca9b521405ef4453a2778cb88a659f927
TheVestedLeopard <sachajw@gmail.com>
2021-04-19 18:59:08 +0200

    added pet names

--- k8s/migration/V4__insert_data.sql
+++ k8s/migration/V4__insert_data.sql
@@ -1,13 +1,13 @@
 INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (1, 'George', '110 W. Liberty St.', 'Madison', '6085551023', 'briskey');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (2, 'Betty', '638 Cardinal Ave.', 'Sun Prairie', '6085551749', '');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (3, 'Eduardo', '2693 Commerce St.', 'McFarland', '6085558763');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (4, 'Harold', '563 Friendly St.', 'Windsor', '6085553198');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (5, 'Peter',  '2387 S. Fair Way', 'Madison', '6085552765');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (6, 'Jean',  '105 N. Lake St.', 'Monona', '6085552654');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (7, 'Jeff',  '1450 Oak Blvd.', 'Monona', '6085555387');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (8, 'Maria', '345 Maple St.', 'Madison', '6085557683');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (9, 'David', '2749 Blackhawk Trail', 'Madison', '6085559435');
-INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (10, 'Carlos', '2335 Independence La.', 'Waunakee', '6085555487');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (2, 'Betty', '638 Cardinal Ave.', 'Sun Prairie', '6085551749', 'blaarslaai');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (3, 'Eduardo', '2693 Commerce St.', 'McFarland', '6085558763', 'mr trixie');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (4, 'Harold', '563 Friendly St.', 'Windsor', '6085553198', 'motley cruise');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (5, 'Peter',  '2387 S. Fair Way', 'Madison', '6085552765', 'dune cat');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (6, 'Jean',  '105 N. Lake St.', 'Monona', '6085552654', 'koos');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (7, 'Jeff',  '1450 Oak Blvd.', 'Monona', '6085555387', 'meew-nipulator');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (8, 'Maria', '345 Maple St.', 'Madison', '6085557683', 'what-a-mess');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (9, 'David', '2749 Blackhawk Trail', 'Madison', '6085559435', 'funky dog');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (10, 'Carlos', '2335 Independence La.', 'Waunakee', '6085555487', 'to-many-fleas');
 
 INSERT INTO vets (id, name, specialties) VALUES (1, 'James', 'tech' );
 INSERT INTO vets (id, name, specialties) VALUES (2, 'Helen', 'radiology');
