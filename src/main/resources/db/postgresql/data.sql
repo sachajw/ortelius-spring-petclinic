c74da2e111e260f5f5b72c9d70dc1eb27e694916
TheVestedLeopard <sachajw@gmail.com>
2021-04-27 12:21:08 +0200

    mysql to postgresql

---
+++ src/main/resources/db/postgresql/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (1, 'George', '110 W. Liberty St.', 'Madison', '6085551023', 'briskey');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (2, 'Betty', '638 Cardinal Ave.', 'Sun Prairie', '6085551749', 'blaarslaai');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (3, 'Eduardo', '2693 Commerce St.', 'McFarland', '6085558763', 'mr trixie');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (4, 'Harold', '563 Friendly St.', 'Windsor', '6085553198', 'motley cruise');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (5, 'Peter',  '2387 S. Fair Way', 'Madison', '6085552765', 'dune cat');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (6, 'Jean',  '105 N. Lake St.', 'Monona', '6085552654', 'koos');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (7, 'Jeff',  '1450 Oak Blvd.', 'Monona', '6085555387', 'meew-nipulator');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (8, 'Maria', '345 Maple St.', 'Madison', '6085557683', 'what-a-mess');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (9, 'David', '2749 Blackhawk Trail', 'Madison', '6085559435', 'funky dog');
+INSERT INTO owners (id, name, address, city, telephone, pets) VALUES (10, 'Carlos', '2335 Independence La.', 'Waunakee', '6085555487', 'to-many-fleas');
+
+INSERT INTO vets (id, name, specialties) VALUES (1, 'James', 'tech' );
+INSERT INTO vets (id, name, specialties) VALUES (2, 'Helen', 'radiology');
+INSERT INTO vets (id, name, specialties) VALUES (3, 'Linda', 'dentist surgery');
+INSERT INTO vets (id, name, specialties) VALUES (4, 'Rafael', 'surgery');
+INSERT INTO vets (id, name, specialties) VALUES (5, 'Henry', 'radiology');
+INSERT INTO vets (id, name, specialties) VALUES (6, 'Sharon', 'secretary');
