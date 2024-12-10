1383996a88a909888229425c6d9b5ec68c2550ab
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-12-10 19:08:51 +0200

    🛠 NEW: ortelius config

--- dhenv.sh
+++ dhenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export DHURL=https://ortelius.pangarabbit.com
-export DHUSER=admin
-export DHPASS=admin
-export DOCKERREPO=quay.io/pangarabbit/ortelius-spring-petclinic
-export IMAGE_TAG=1.0.0
+export DHURL="https://ortelius.pangarabbit.com"
+export DHUSER="walle"
+export DHPASS="Whimsical-Claim-Selective6"
+export DOCKERREPO="quay.io/pangarabbit/ortelius-spring-petclinic"
+export IMAGE_TAG="1.0.0"
